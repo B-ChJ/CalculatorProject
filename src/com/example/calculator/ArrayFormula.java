bf5865a614f4b04e9b444bf184f9d81af7b01c8c
B-ChJ <bchaejoo@gmail.com>
2025-09-16 21:27:31 +0900

    Add ArrayFormula Class - Issue 1.

---
+++ src/com/example/calculator/ArrayFormula.java
@@ -0,0 +1,14 @@
+package com.example.calculator;
+
+
+
+public class ArrayFormula {
+    //1. 속성 = 입력받은 값과 연산자
+    //double num1, num2;
+    char  num1, num2, operator;
+    //2. 생성자 = 배열 형태로 정렬
+    public ArrayFormula(char num1, char num2, char operator) {
+        //char 자료형을 string으로 입력받기?
+        char[] arrBefore = char.toString();
+    }
+}
