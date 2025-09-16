12a922bbd7ee0a2e4b78269c6abe4fe0c222d202
B-ChJ <bchaejoo@gmail.com>
2025-09-16 20:32:10 +0900

    Add main class. Add Calculation method to Calculator Class. (add, minus, multiply, divide, mod, square)

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,4 +1,30 @@
 package com.example.calculator;
 
 public class Calculator {
+    double current;
+
+    public double add(double num1, double num2){
+        current = num1 + num2;
+        return current;
+    }
+    public double minus(double num1, double num2){
+        current = num1 - num2;
+        return current;
+    }
+    public double multiply(double num1, double num2){
+        current = num1 * num2;
+        return current;
+    }
+    public double divide(double num1, double num2){
+        current = num1 / num2;
+        return current;
+    }
+    public double mod(double num1, double num2){
+        current = num1 % num2;
+        return current;
+    }
+    public double square(double num1){
+        current = num1 * num1;
+        return current;
+    }
 }
---
+++ src/com/example/calculator/main.java
@@ -0,0 +1,9 @@
+package com.example.calculator;
+
+public class main {
+    public static void main(String[] args) {
+        double current = 0;
+                
+        System.out.println(current);
+    }
+}
