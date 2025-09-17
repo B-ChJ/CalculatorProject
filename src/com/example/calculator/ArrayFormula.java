650bbdad4a2c4a6305e8cb9447b601034b0890ca
B-ChJ <bchaejoo@gmail.com>
2025-09-17 21:18:25 +0900

    Rename arrAfter → arrMiddle - Object를 사용하기 전과 후를 구분하기 위함. Change return type(Array → Object) - ArrayFormula.java Add (String → Double) Code to ArrayFormula.java Add Exception(Incomplete) to Main.java

--- src/com/example/calculator/ArrayFormula.java
+++ src/com/example/calculator/ArrayFormula.java
@@ -1,16 +1,31 @@
 package com.example.calculator;
 
+import java.lang.Exception;
+import static java.lang.Double.parseDouble;
 
 public class ArrayFormula {
-    //1. 속성 = 입력받은 값과 연산자
-    //String arrBefore;
-    //2. 생성자 = 배열 형태로 정렬
-    public String[] makeArray(String arrBefore) {
+    //1. 속성 = 입력받은 값과 연산자(생략)
+
+    //2. 생성자 = Calculator에 보낼 배열
+    ArrayFormula arrBefore =  new ArrayFormula();
+
+    //3. 기능 = 문자열 to 배열로 변환
+    public Object[] makeArray(String arrBefore) {
         //문자열을 연산자 기준으로 나누기
-        String[] arrAfter = arrBefore.split("[+\\-*/%^=]");
-        //배열 잘 나눠지는지 확인용 출력
-        for (String s : arrAfter) {
-            System.out.println(s);
+        String[] arrMiddle = arrBefore.split("[+\\-*/%^=]");
+
+        //arrMiddle 중 실수를 의미하는 값을 문자열에서 실수형으로 바꾸기
+        //실수형과 문자형이 같이 있는 배열 Object에 대입
+        Object[] arrAfter = new Object[arrMiddle.length];
+        for (int i = 0; i < arrMiddle.length; i++) {
+            if((arrMiddle[i] == "+") || (arrMiddle[i] == "-") || (arrMiddle[i] == "*") || (arrMiddle[i] == "/") || (arrMiddle[i] == "%") || (arrMiddle[i] == "^")) {
+                arrAfter[i] = arrMiddle[i];
+            }
+            //문자 "="는 배열에 포함❌(저장소 낭비)
+            else if (arrMiddle[i] == "=") { }
+            //실수는 형 변환해서 입력
+            else {
+                arrAfter[i] = Double.parseDouble(arrMiddle[i]); }
         }
         return arrAfter;
     }
--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -1,5 +1,7 @@
 package com.example.calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.String;
 
@@ -7,11 +9,23 @@ import java.lang.String;
 
 public class Main {
     public static void main(String[] args) {
-        double current = 0;
+        String current = "";
         Scanner scanner = new Scanner(System.in);
         String arrBefore = scanner.nextLine();
+        //입력받은 문자열 나누기 → ArrayFormula.java
+        ArrayFormula arr =  new ArrayFormula();
+        Object[] arrAfter = arr.makeArray(arrBefore);
 
-        //        System.out.println(new ArrayFormula(arrBefore));
+        //예외 처리
+            int i;
+            for (i = 0; i < arrAfter.length - 1; i++) {
+                if (arrAfter[i].toString() == "/" && arrAfter[i + 1].toString() == "0") {
+                    System.out.println("ERROR");
+                }
+            }
+
+        //arrAfter 배열을 Calculator로 보내 연산 작업을 하고 결과값 current를 받는다.
+
         System.out.println(current);
         }
     }
