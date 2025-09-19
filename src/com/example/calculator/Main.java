d98221ddfc36495e650ffac740638b2be15e4d6f
B-ChJ <bchaejoo@gmail.com>
2025-09-19 11:26:43 +0900

    current에 추가할 연산 작업을 반복할 수 있도록 while문 + 입력 과정 추가

--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -20,81 +20,89 @@ public class Main {
                 System.out.println("ERROR");
             }
         }
-
-        //arrAfter 배열을 Calculator로 보내 연산 작업을 하고 결과값 current를 받는다.
-        while (arrAfter.size() > 1) {
-            Calculation calc = new Calculation();
-            int n = 0;
-            char operator;
-            //연산자 우선순위(^*/% → +-) 순서대로 연산
-            //연산이 끝나면 사용했던 index를 삭제하고 결과값 current를 추가
-            if (arrAfter.contains("^")) {
-                operator = '^';
-                n = arrAfter.indexOf("^");
-                double num1 = Double.parseDouble(arrAfter.get(n - 1));
-                double num2 = Double.parseDouble(arrAfter.get(n + 1));
-                current = calc.CalculatePackage(num1, num2, operator);
-                arrAfter.set(n, String.valueOf(current));
-                arrAfter.remove(n - 1);
-                arrAfter.remove(n);
+        boolean power = true;
+        while (power) {
+            //arrAfter 배열을 Calculator로 보내 연산 작업을 하고 결과값 current를 받는다.
+            while (arrAfter.size() > 1) {
+                Calculation calc = new Calculation();
+                int n = 0;
+                char operator;
+                //연산자 우선순위(^*/% → +-) 순서대로 연산
+                //연산이 끝나면 사용했던 index를 삭제하고 결과값 current를 추가
+                if (arrAfter.contains("^")) {
+                    operator = '^';
+                    n = arrAfter.indexOf("^");
+                    double num1 = Double.parseDouble(arrAfter.get(n - 1));
+                    double num2 = Double.parseDouble(arrAfter.get(n + 1));
+                    current = calc.CalculatePackage(num1, num2, operator);
+                    arrAfter.set(n, String.valueOf(current));
+                    arrAfter.remove(n - 1);
+                    arrAfter.remove(n);
+                } else if (arrAfter.contains("*")) {
+                    operator = '*';
+                    n = arrAfter.indexOf("*");
+                    double num1 = Double.parseDouble(arrAfter.get(n - 1));
+                    double num2 = Double.parseDouble(arrAfter.get(n + 1));
+                    current = calc.CalculatePackage(num1, num2, operator);
+                    arrAfter.set(n, String.valueOf(current));
+                    arrAfter.remove(n - 1);
+                    arrAfter.remove(n);
+                } else if (arrAfter.contains("/")) {
+                    operator = '/';
+                    n = arrAfter.indexOf("/");
+                    double num1 = Double.parseDouble(arrAfter.get(n - 1));
+                    double num2 = Double.parseDouble(arrAfter.get(n + 1));
+                    current = calc.CalculatePackage(num1, num2, operator);
+                    arrAfter.set(n, String.valueOf(current));
+                    arrAfter.remove(n - 1);
+                    arrAfter.remove(n);
+                } else if (arrAfter.contains("%")) {
+                    operator = '%';
+                    n = arrAfter.indexOf("%");
+                    double num1 = Double.parseDouble(arrAfter.get(n - 1));
+                    double num2 = Double.parseDouble(arrAfter.get(n + 1));
+                    current = calc.CalculatePackage(num1, num2, operator);
+                    arrAfter.set(n, String.valueOf(current));
+                    arrAfter.remove(n - 1);
+                    arrAfter.remove(n);
+                } else {
+                    if (arrAfter.contains("+")) {
+                        operator = '+';
+                        n = arrAfter.indexOf("+");
+                        double num1 = Double.parseDouble(arrAfter.get(n - 1));
+                        double num2 = Double.parseDouble(arrAfter.get(n + 1));
+                        current = calc.CalculatePackage(num1, num2, operator);
+                        arrAfter.set(n, String.valueOf(current));
+                        arrAfter.remove(n - 1);
+                        arrAfter.remove(n);
+                    } else if (arrAfter.contains("-")) {
+                        operator = '-';
+                        n = arrAfter.indexOf("-");
+                        double num1 = Double.parseDouble(arrAfter.get(n - 1));
+                        double num2 = Double.parseDouble(arrAfter.get(n + 1));
+                        current = calc.CalculatePackage(num1, num2, operator);
+                        arrAfter.set(n, String.valueOf(current));
+                        arrAfter.remove(n - 1);
+                        arrAfter.remove(n);
+                    }
+                }
+                //연산 과정 확인용 출력
+                //System.out.println(arrAfter);
+                //System.out.println(calc.current);
             }
-            else if (arrAfter.contains("*")) {
-                operator = '*';
-                n = arrAfter.indexOf("*");
-                double num1 = Double.parseDouble(arrAfter.get(n - 1));
-                double num2 = Double.parseDouble(arrAfter.get(n + 1));
-                current = calc.CalculatePackage(num1, num2, operator);
-                arrAfter.set(n, String.valueOf(current));
-                arrAfter.remove(n - 1);
-                arrAfter.remove(n);
-            }
-            else if (arrAfter.contains("/")) {
-                operator = '/';
-                n = arrAfter.indexOf("/");
-                double num1 = Double.parseDouble(arrAfter.get(n - 1));
-                double num2 = Double.parseDouble(arrAfter.get(n + 1));
-                current = calc.CalculatePackage(num1, num2, operator);
-                arrAfter.set(n, String.valueOf(current));
-                arrAfter.remove(n - 1);
-                arrAfter.remove(n);
-            }
-            else if (arrAfter.contains("%")) {
-                operator = '%';
-                n = arrAfter.indexOf("%");
-                double num1 = Double.parseDouble(arrAfter.get(n - 1));
-                double num2 = Double.parseDouble(arrAfter.get(n + 1));
-                current = calc.CalculatePackage(num1, num2, operator);
-                arrAfter.set(n, String.valueOf(current));
-                arrAfter.remove(n - 1);
-                arrAfter.remove(n);
+            System.out.println("결과 : " + current);
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료)\n" + current);
+            Scanner scanner2 = new Scanner(System.in);
+            String answer = scanner2.nextLine();
+            if (answer.equals("exit")) {
+                power = false;
             }
             else {
-                if (arrAfter.contains("+")) {
-                    operator = '+';
-                    n = arrAfter.indexOf("+");
-                    double num1 = Double.parseDouble(arrAfter.get(n - 1));
-                    double num2 = Double.parseDouble(arrAfter.get(n + 1));
-                    current = calc.CalculatePackage(num1, num2, operator);
-                    arrAfter.set(n, String.valueOf(current));
-                    arrAfter.remove(n - 1);
-                    arrAfter.remove(n);
-                }
-                else if (arrAfter.contains("-")) {
-                    operator = '-';
-                    n = arrAfter.indexOf("-");
-                    double num1 = Double.parseDouble(arrAfter.get(n - 1));
-                    double num2 = Double.parseDouble(arrAfter.get(n + 1));
-                    current = calc.CalculatePackage(num1, num2, operator);
-                    arrAfter.set(n, String.valueOf(current));
-                    arrAfter.remove(n - 1);
-                    arrAfter.remove(n);
-                }
+                ArrayFormula arr2 = new ArrayFormula();
+                ArrayList<String> arrAfter2 = arr2.makeArray(answer);
+                arrAfter.addAll(arrAfter2);
             }
-            //연산 과정 확인용 출력
-            //System.out.println(arrAfter);
-            //System.out.println(calc.current);
         }
-        System.out.println("결과 : " + current);
     }
 }
 
